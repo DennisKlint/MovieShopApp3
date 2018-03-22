3bed937290ff71f7ec011a54bf6d657a3d8d0dcc
sahlinstefan <34234973+sahlinstefan@users.noreply.github.com>
2018-03-22 09:56:34 +0100

    Gui update, Admin dropdown
    Added dropdown for admin
    Added style to actionlinks (bootstrap default buttons)

--- MovieShopApp3/Controllers/CategoriesController.cs
+++ MovieShopApp3/Controllers/CategoriesController.cs
@@ -1,45 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MovieShopApp3.Models;
+
 namespace MovieShopApp3.Controllers
 {
-   
     public class CategoriesController : Controller
     {
         private dbMSA3Entities db = new dbMSA3Entities();
+
         // GET: Categories
         public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+        public ActionResult PartialViewCategories()
         {
             var cat = db.Categories;
 
             return PartialView("PartialViewCategories", cat.ToList());
 
         }
-        //public ActionResult PartialViewCategories()
-        //{
-        //    var cat = db.Categories;
-
-        //    return PartialView("PartialViewCategories",cat.ToList());
-           
-        //}
         public ActionResult showByCategory(int id)
         {
             Session["selectedCat"] = id;
-            List <Products> productlist = new List<Products>();
-            productlist = db.Products.SqlQuery("Select Products.* from Products,ProdCat where Products.ProductID = ProdCat.ProductID AND ProdCat.CategoryID ='"  + id + "'").ToList();
+            List<Products> productlist = new List<Products>();
+            productlist = db.Products.SqlQuery("Select Products.* from Products,ProdCat where Products.ProductID = ProdCat.ProductID AND ProdCat.CategoryID ='" + id + "'").ToList();
 
 
-            return View("../Products/PartialView_Products",productlist);
+            return View("../Products/PartialView_Products", productlist);
 
         }
-        
-        // GET: Categories/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Purchase(int id)
         {
-            return View();
+            if (Session["CartList"] == null)
+            {
+                List<int> cartList = new List<int>();
+                Session["CartList"] = cartList;
+            }
+
+            var cartlist = (List<int>)Session["CartList"];
+            cartlist.Add(id);
+            Session["CartList"] = cartlist;
+            return showByCategory(Convert.ToInt32(Session["selectedCat"]));
+        }
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = db.Categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
         }
 
         // GET: Categories/Create
@@ -49,81 +71,86 @@ namespace MovieShopApp3.Controllers
         }
 
         // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Categories categories)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
-
+                db.Categories.Add(categories);
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+
+            return View(categories);
         }
 
         // GET: Categories/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = db.Categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
         }
 
         // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Categories categories)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
-
+                db.Entry(categories).State = EntityState.Modified;
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View(categories);
         }
 
         // GET: Categories/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = db.Categories.Find(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
         }
 
         // POST: Categories/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
         {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            Categories categories = db.Categories.Find(id);
+            db.Categories.Remove(categories);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Purchase(int id)
+        protected override void Dispose(bool disposing)
         {
-            if (Session["CartList"] == null)
+            if (disposing)
             {
-                List<int> cartList = new List<int>();
-                Session["CartList"] = cartList;
+                db.Dispose();
             }
-           
-
-
-            var cartlist = (List<int>)Session["CartList"];
-            cartlist.Add(id);
-            Session["CartList"] = cartlist;
-           return  showByCategory(Convert.ToInt32(Session["selectedCat"]));
-            //return Redirect("~/Products/Index");
-            //return RedirectToAction("PartialViewCategories");
+            base.Dispose(disposing);
         }
     }
 }
